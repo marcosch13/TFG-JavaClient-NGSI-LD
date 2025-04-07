33e7d0b1ab528f7baf8c5904b699b61f719a3865
Marcos Chamorro <marcos.chamorro@alumnos.upm.es>
2025-04-07 11:34:36 +0200

    primer paso

--- ngsi-ld-client/src/main/java/mi_proyecto/CreateIotEntity.java
+++ ngsi-ld-client/src/main/java/mi_proyecto/CreateIotEntity.java
@@ -1,5 +1,60 @@
 package mi_proyecto;
 
+import org.openapitools.client.api.ContextInformationProvisionApi;
+import org.openapitools.client.model.QueryEntity200ResponseInner;
+
+import com.google.gson.Gson;
+
+import org.openapitools.client.model.IotDevice;
+import org.openapitools.client.model.IotDescription;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.Configuration;
+
+import java.net.URI;
+
+public class CreateIotEntity {
+    public static void main(String[] args) {
+        try {
+
+            String idNumero = (args != null && args.length > 0) ? args[0] : "1";
+            int num = Integer.parseInt(idNumero);
+            String idFormateado = String.format("%03d", num);
+
+            //Creo entidad IotDevice
+            IotDevice device = new IotDevice();
+            device.setId(new URI("urn:ngsi-ld:IotDevice:" + idFormateado));
+            device.setType(IotDevice.TypeEnum.IOT_DEVICE);
+            device.setDescription(new IotDescription().value("IoT device **************"));
+
+            //Convertir a JSON
+            String json = device.toJson();
+            System.out.println("Payload JSON:\n" + json);
+
+            //Paso a entidad NGSI-LD genérica
+            QueryEntity200ResponseInner entity = QueryEntity200ResponseInner.fromJson(json);
+
+            // Crear cliente y API
+            ApiClient apiClient = Configuration.getDefaultApiClient();
+            apiClient.setBasePath("http://localhost:1026/ngsi-ld/v1");
+            ContextInformationProvisionApi api = new ContextInformationProvisionApi(apiClient);
+
+            // Crear la entidad usando la API
+            api.createEntity(false, null, null, entity);
+
+
+            //obtener respuesta del context broker y mostrarla
+            
+            System.out.println("Entidad creada correctamente: " + device.getId());
+
+        } catch (Exception e) {
+            System.err.println("Error al crear la entidad vía API:");
+            e.printStackTrace();
+        }
+    }
+}
+
+/* 
+
 import org.openapitools.client.model.IotDevice;
 import org.openapitools.client.model.IotDescription;
 
@@ -51,4 +106,4 @@ public class CreateIotEntity{
             e.printStackTrace();
         }
     }
-}
+}  */
