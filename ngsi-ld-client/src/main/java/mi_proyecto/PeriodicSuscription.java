442349322a66acbc50d075f5f66711e29f977503
Marcos Chamorro <marcos.chamorro@alumnos.upm.es>
2025-06-18 17:55:19 +0200

    cambio suscperiodica

--- ngsi-ld-client/src/main/java/mi_proyecto/PeriodicSuscription.java
+++ ngsi-ld-client/src/main/java/mi_proyecto/PeriodicSuscription.java
@@ -19,6 +19,7 @@ public class PeriodicSuscription {
     public static void main(String[] args) {
 
         try {
+            Scanner scanner = new Scanner(System.in);
 
             ApiClient apiClient = Configuration.getDefaultApiClient();
             apiClient.setBasePath("http://localhost:9090/ngsi-ld/v1");
@@ -40,37 +41,44 @@ public class PeriodicSuscription {
 
             //profe: attributes=["temperature", "humidity"], sysAttrs=True
             
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("A que tipo de sensor quieres suscribirte? \n humedad \n temperatura ");
+            System.out.println("A que tipo de entidad quieres suscribirte? \n Iot \n humedad \n temperatura ");
             String tipoEntidad = scanner.nextLine();
+            String letra = "";
 
             String tipo = "";
             if(tipoEntidad.equals("humedad")){
                 tipo = "HumiditySensor";
+                letra = "H";
             }else if(tipoEntidad.equals("temperatura")){
                 tipo = "TemperatureSensor";
+                letra = "T";
+            }else if(tipoEntidad.equals("Iot")){
+                tipo = "IotDevice";
+                letra = "I";
             }
-            EntitySelector Entity = new EntitySelector();
-            Entity.setType(tipo);
+            
 
             System.out.println("Introduce el número final de ID de la entidad: ");
             String NumeroId = scanner.nextLine();
+            String NumeroIdFormateado = String.format("%03d", Integer.parseInt(NumeroId));
+            URI entityUri = new URI("urn:ngsi-ld:" + tipo + ":"  +NumeroIdFormateado);
 
-            String letra = "";
-            if (tipoEntidad.equals("humedad")) {
-                letra = "H";
-            }else if (tipoEntidad.equals("temperatura")) {
-                letra = "T";
-            }
+            System.out.println("Introduce el periodo de la subscripción en segundos: ");
+            String periodo = scanner.nextLine();
+            BigDecimal periodoBigDecimal = new BigDecimal(periodo);
+
+            EntitySelector Entity = new EntitySelector();
+            Entity.setType(tipo);
+            //Entity.setId(entityUri);
 
             CreateSubscriptionRequest subscription = new CreateSubscriptionRequest();
 
             subscription.setType(CreateSubscriptionRequest.TypeEnum.SUBSCRIPTION);
-            subscription.setId(new URI("urn:ngsi-ld:Subscription:Periodic:" + letra + NumeroId));
+            //subscription.setId(new URI("urn:ngsi-ld:Subscription:Periodic:" + letra + NumeroId));
             subscription.setNotification(notificationParams);
             subscription.setEntities(Arrays.asList(Entity));
             subscription.setDescription("Subscripción periodica a :");
-            subscription.setTimeInterval(new BigDecimal(10));//esepcificar por el usuario
+            subscription.setTimeInterval(periodoBigDecimal);
             subscription.setIsActive(true);
             subscription.setNotificationTrigger(null);
             subscription.setWatchedAttributes(null);
@@ -78,7 +86,7 @@ public class PeriodicSuscription {
             System.out.println("Suscripción JSON:\n" + subscription.toJson());
 
             ApiResponse<Void> response = subscriptionApi.createSubscriptionWithHttpInfo(null, null, null, subscription);
-            System.out.println("Response: " + response.getData());
+            System.out.println("Response: " + response.getStatusCode());
             
         } catch (Exception e) {
             e.printStackTrace();
